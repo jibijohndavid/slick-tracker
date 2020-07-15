4873687e16f28e89f71a1031629ad9c507260ab2
Jibi John David <jibijohndavid@gmail.com>
2020-07-16 01:28:05 +0530

    added main layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 
 import theme from './theme';
+import MainLayout from './layouts/MainLayout';
 import Routes from './Routes';
 
 function App(): React.ReactElement {
   return (
     <ThemeProvider theme={theme}>
-      <Routes />
+      <MainLayout>
+        <Routes />
+      </MainLayout>
     </ThemeProvider>
   );
 }
