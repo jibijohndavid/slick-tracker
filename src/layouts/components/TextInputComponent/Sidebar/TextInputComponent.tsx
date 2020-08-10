e484324ec538504358caffc9775c9955c111eb31
Harisha <harisha.ph@pitsolutions.com>
2020-08-10 15:21:02 +0530

    TextField and Notification Component

---
+++ src/layouts/components/TextInputComponent/Sidebar/TextInputComponent.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { TextField } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
+
+
+interface IProps {
+  labelText: string;
+  placeholderText: string;
+  inputType?: string;
+}
+
+const TextInputComponent = (props: IProps) => {
+  const { labelText, placeholderText, inputType } = props;
+
+  const CustomTextField = withStyles({
+    root: {
+      '& label.Mui-focused': {
+        color: '#002b5d',
+      },
+      '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+          borderColor: '#cacfd9',
+        },
+        '&:hover fieldset': {
+          borderColor: '#3384f3',
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: '#3384f3',
+        },
+      },
+    },
+  })(TextField);
+
+  return (
+    <div>
+      <p style={{ fontSize: 12, color: '#002b5d', fontWeight: 'bolder' }}>{labelText}</p>
+      <CustomTextField
+        placeholder={placeholderText}
+        type={inputType}
+        variant='outlined'
+        size='small'
+        {...props}
+      />
+    </div >
+  );
+};
+
+export default TextInputComponent;
