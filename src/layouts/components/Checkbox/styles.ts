2b5cd8feecd2891313c664707aef204a040166b5
unknown <sreehari9011@gmail.com>
2020-08-10 16:04:33 +0530

    checkbox completed

---
+++ src/layouts/components/Checkbox/styles.ts
@@ -0,0 +1,14 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    formControl: {
+      margin: theme.spacing(3),
+    },
+  }),
+);
+
+export default useStyles;
